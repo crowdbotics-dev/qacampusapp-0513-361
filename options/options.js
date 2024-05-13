e42c6f47f736f412279081271f64563561d67882
Crowdbotics <engineering@crowdbotics.com>
2024-05-13 15:30:25 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "qacampusapp-0513-361",
+  url: "https://qacampusapp-0513-361.botics.co",
+  api: "https://qacampusapp-0513-361.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
